46812f72d1db9d55f8bb86436bcc13f8a146b846
SocialEgale.AI <upskill@socialeagle.ai>
2025-07-07 12:30:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'SocialEagle.AI',
+  description: 'Created By SocialEagle.AI',
+  generator: 'SocialEagle.AI',
 }
 
 export default function RootLayout({
